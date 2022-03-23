0add30ec8983a427ab0b6f31094dfb82697bed6f
andreavolpato <44768887+andreavolpato@users.noreply.github.com>
2022-03-23 11:09:50 +0100

    Update Bootstrap to 5.1.3 and Bootstrap Icons to 1.8.1

--- includes/head.php
+++ includes/head.php
@@ -9,8 +9,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 
     <!-- CSS Styles, including Bootstrap and Bootstrap-icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Bootstrap from https://getbootstrap.com/ and use 'CDN via jsDelivr' from download page -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap icons from https://icons.getbootstrap.com/ using 'CDN' from install section -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- Custom stylesheet -->
     <link href="scss/styles.css" rel="stylesheet"/>
 
     <title>TestaLab</title>
