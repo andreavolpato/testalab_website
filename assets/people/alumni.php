e79bb84da19e060b4a3a3f5480e1f2601723c067
andreavolpato <44768887+andreavolpato@users.noreply.github.com>
2022-06-15 13:37:25 +0200

    Update alumni.php

--- assets/people/alumni.php
+++ assets/people/alumni.php
@@ -1,15 +1,16 @@
+Anne Auer, Intern and Master Student<br>
 Elham Jalalvand, Postdoctoral Researcher<br>
 Staffan Al-Kadhimi, Intern and Collaborator<br>
 Lea Rems, Postdoctoral Researcher<br>
-Debu, Intern<br>
-Sophie, Intern<br>
-Joseph, Intern<br>
+Debashish Chitnis, Intern<br>
+Sophie Kopetschke, Intern<br>
+Josep, Intern<br>
 Andrea Bucci, Master Student<br>
 Federico Barabas, Postdoctoral Researcher<br>
 Jes Dreier, Postdoctoral Researcher<br>
 Luciano Masullo, Visiting PhD Student<br>
 Elin Sandberg, Master Student<br>
 Aurelien Barbotin, Master Student<br>
-Eleonora Uriati, Master student<br>
-Francesco Fersini, Master student<br>
+Eleonora Uriati, Master Student<br>
+Francesco Fersini, Master Student<br>
 Shusei Masuda, Master Student<br>
